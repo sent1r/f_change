0d1e66e06de893b5a05d52057b4cd32d0c807a7c
Alexandr Smolsky <asmolsky@beltechsoftware.com>
2018-06-21 15:32:03 +0300

    #6 Initial Commit. Assets4.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module FChange
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib/modules')
     config.eager_load_paths << Rails.root.join('lib/modules')
+    config.assets.precompile << %w( *.scss *.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
