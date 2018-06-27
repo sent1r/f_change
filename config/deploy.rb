1af585396c060785b42442c7ef157212969e66b1
Alexandr Smolsky <asmolsky@beltechsoftware.com>
2018-06-27 18:06:13 +0300

    #9 Set Capistrano gems fix.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.11.0"
 set :application, 'f_change'
 set :repo_url, 'git@github.com:sent1r/f_change.git'
 
-set :deploy_to, '/home/deploy/www/public'
+set :deploy_to, '/home/deploy/www/public/f_change'
 
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
